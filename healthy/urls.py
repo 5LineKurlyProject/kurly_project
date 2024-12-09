3186b8cd5e7642278a4b6ea527d5f6888f44d386
YEERRin <ryeon0107@gmail.com>
2024-12-09 12:35:04 +0900

    [feat] 건강식품 검색/검색 결과 페이지 나누기 및 top 4개 상품 보여주기 추가

--- healthy/urls.py
+++ healthy/urls.py
@@ -4,7 +4,8 @@ from . import views
 urlpatterns = [
     
     #루트 경로로 들어왔을 경우, 검색 페이지를 보여줌 
-    path("", views.search, name='search'),
+    path("", views.main_page, name='main_page'),
+    path("search", views.search, name='search'),
     path("product/<int:product_id>/", views.product_reviews, name='product_reviews'),
     path('product/<int:product_id>/wordcloud/', views.product_reviews_wordcloud, name='product_reviews_wordcloud'),
 ]
