4ed4523dae74731cc9d56d1e350e3f2bfc0e8183
YEERRin <ryeon0107@gmail.com>
2024-12-09 23:17:20 +0900

    url 동일 이름 변경

--- healthy/urls.py
+++ healthy/urls.py
@@ -4,8 +4,8 @@ from healthy import views
 urlpatterns = [
     
     #루트 경로로 들어왔을 경우, 검색 페이지를 보여줌 
-    path("", views.main_page, name='main_page'),
-    path("search", views.search, name='search'),
-    path("product/<int:product_id>/", views.product_reviews, name='product_reviews'),
-    path('product/<int:product_id>/wordcloud/', views.product_reviews_wordcloud, name='product_reviews_wordcloud'),
+    path("", views.main_page, name='healthy/main_page'),
+    path("search", views.search, name='healthy/search'),
+    path("product/<int:product_id>/", views.product_reviews, name='healthy/product_reviews'),
+    path('product/<int:product_id>/wordcloud/', views.product_reviews_wordcloud, name='healthy/product_reviews_wordcloud'),
 ]
