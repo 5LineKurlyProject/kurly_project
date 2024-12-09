bf466fe772c01ce3807cb6e6b94a49d71fac7cf9
Gyoung_0 <hkhj77777@gmail.com>
2024-12-09 23:24:09 +0900

    reapair: url 이름 변경

--- makeup/urls.py
+++ makeup/urls.py
@@ -2,8 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.main_page, name="main_page"),
-    path("search/", views.search, name="search_results"),
-    path("product/<int:product_id>/", views.product_reviews, name="product_reviews"),
-    path("product/<int:product_id>/wordcloud/", views.product_reviews_wordcloud, name="product_reviews_wordcloud"),
+    path("", views.main_page, name="main_page_make"),
+    path("search/", views.search, name="search_results_make"),
+    path("product/<int:product_id>/", views.product_reviews, name="product_reviews_make"),
+    path("product/<int:product_id>/wordcloud/", views.product_reviews_wordcloud, name="product_reviews_wordcloud_make"),
 ]
