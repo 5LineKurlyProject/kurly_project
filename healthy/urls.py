121b4f66fa543086888e5e8ce4600a11535769c6
YEERRin <ryeon0107@gmail.com>
2024-12-09 10:20:34 +0900

    [feat] 건강식품 카테고리 상품 검색 및 시각화

---
+++ healthy/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    
+    #루트 경로로 들어왔을 경우, 검색 페이지를 보여줌 
+    path("", views.search, name='search'),
+    path("product/<int:product_id>/", views.product_reviews, name='product_reviews'),
+    path('product/<int:product_id>/wordcloud/', views.product_reviews_wordcloud, name='product_reviews_wordcloud'),
+]
